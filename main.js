0d6b1a9406e7011c3a5eb5f1add18898e94b943b
meepstertron <62438310+meepstertron@users.noreply.github.com>
2025-08-04 00:30:34 +0200

    added dev tools

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 console.log("Slack-Ingoff Loaded! :D")
+
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'F12') {
+    // Try to open dev tools if possible
+    if (window.require) {
+      const { remote } = window.require('electron');
+      remote.getCurrentWindow().webContents.openDevTools();
+    } else {
+      console.log('Electron remote module not accessible');
+    }
+  }
+});
