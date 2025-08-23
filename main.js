8d4ed54a44f4ac8137739582e8af7a55b7ef8b95
meepstertron <meepstertron@gmail.com>
2025-08-23 15:25:02 +0200

    uhh stuff

--- main.js
+++ main.js
@@ -66,19 +66,45 @@ window.addEventListener("load", () => {
       if (child) {
         child.document.write(`
           <!DOCTYPE html>
-          <html lang="en">
-          <head>
-            <meta charset="UTF-8">
-            <title>Config</title>
-            <style>
-              body { font-family: sans-serif; padding: 2em; background: #fafafa; }
-              h1 { color: #611f69; }
-            </style>
-          </head>
-          <body>
-            <h1>Test :D</h1>
-          </body>
-          </html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Config</title>
+    <style></style>
+  </head>
+  <body>
+    <div style="margin-bottom: 20px;">
+      <span style="font-size: xx-large;">Change yo settings</span> v0.0.1
+    </div>
+
+    
+
+    <section style="outline: 1px dashed #ccc; padding: 5px;">
+        <span style="font-size: larger;">Oooo Themes :D <button class="refetch-themes">Refetch from repo</button></span>
+        <div class="theme-list" style="display: grid; grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); gap: 10px;">
+            <div class="theme-entry" style="outline: 1px solid;"><span>themename</span><span> by author</span><button>Apply theme</button></div>
+        </div>
+
+    </section>
+  </body>
+  <script>
+    const fetchThemeList = async () => {
+      try {
+        const response = await fetch("https://raw.githubusercontent.com/meepstertron/slack-ingoff/2e114b8aa38c1e2e77a1e4f0b6cb194d11d29b96/themes/themes.json");
+        const themes = await response.json();
+        console.log("Fetched themes:", themes);
+      } catch (error) {
+        console.error("Error fetching themes:", error);
+      }
+    };
+
+    const button = document.querySelector(".refetch-themes");
+    button.addEventListener("click", () => {
+      
+    });
+  </script>
+</html>
+
         `);
         child.document.close();
       }
@@ -88,5 +114,12 @@ window.addEventListener("load", () => {
   tryInjectButton();
 });
 
+
+window.loadCSS = (href) => {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = href;
+  document.head.appendChild(link);
+};
 
 
